49c5bbe404879ee5e90e5be5213e7b060252bc80
fic <fic@NOTE06-S28>
2025-08-27 21:27:58 -0300

    Atualizar

--- Projeto DDL.sql
+++ Projeto DDL.sql
@@ -29,10 +29,58 @@ create table ecommerce.pedido (
 		valor_total numeric (14,4) not null,
 		status text not null
 );
+create table ecommerce.pagamento(
+	   id_pagamento int primary key generated always as identity,
+	   id_pedido int,
+	   foreign key (id_pedido) references ecommerce.pedido(id_pedido),
+	   forma_pagamento text not null,
+	   status text not null,
+	   DATA_PAGAMENTO timestamptz not null 	
+);
+create table ecommerce.item_do_pedido(
+	   id_item int,
+	   id_produto int,
+	   foreign key (id_produto) references ecommerce.produto(id_produto),
+	   id_pedido int references ecommerce.pedido (id_pedido),
+	   quantidade int not null
 
+);
+alter table ecommerce.cliente
+add column cpf varchar (14) unique;
 -- APAGAR TABELA - DROP TABLE
 /*DROP TABLE ecommerce.cliente;
 drop table ecommerce.produto;
 drop table ecommerce.pedido;
+alter table
+rename to
+renomear tabela
 */
+create schema clinica;
+create table clinica.medico (
+    id_medico int primary key generated always as identity,
+	nome text not null,
+	crm text not null,
+	especialidade text not null
+);
+create table clinica.paciente (
+    cpf varchar (11) unique,
+	nome text not null,
+	idade int not null,
+	data_nascimento timestamptz not null
+);
+
+create table clinica.clinica (
+    id_clinica int primary key generated always as identity,
+	nome text not null,
+	endereco text not null,
+	descricao text
+);
+create table clinica.consulta (
+    id_consulta int primary key generated always as identity,
+	data timestamptz not null,
+	id_medico int references clinica.medico (id_medico),
+	id_clinica int references clinica.clinica (id_clinica),
+	cpf varchar (11) not null references clinica.paciente (cpf),
+	valor numeric (10,4)
+);
 
